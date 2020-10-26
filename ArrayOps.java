f5961158d71c6043123ec106bd8fac743e32193c
AnnabelZhang1 <azhang22@stuy.edu>
2020-10-25 21:05:15 -0400

    Added more

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,11 +1,46 @@
-public class ArrayOps{}
+public class ArrayOps{
 
   public static int sum(int[] arr)
     {
       int sum = 0;
       for (int i = 0; i < arr.length; i++)
-        sum += Integer.parseInt(arr[i]);
+        sum += arr[i];
       return sum;
     }
 
+  public static int largest(int[] arr)
+    {
+      int max = 0;
+      for (int i = 0; i < arr.length; i++)
+        {
+          max = Math.max(arr[i], max);
+        }
+      return max;
+    }
+
+  public static int[] sumRows(int[][] matrix)
+    {
+      int[] arraysum = new int[matrix[0].length];
+      for (int i = 0; i < matrix.length; i++)
+        {
+          int indivsum = 0;
+          for (int j = 0; j < matrix.length; j++)
+            {
+              indivsum += matrix[i][j];
+            }
+          arraysum[i] = indivsum;
+        }
+      return arraysum;
+    }
+/*
+  public static int[]largestInRows(int[][] matrix)
+    {
+
+    }
+
+  public static sum(int[][] arr)
+    {
+
+    }
+*/
 }
