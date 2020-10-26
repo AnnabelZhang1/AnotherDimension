86f7958ffd4e97d87a4b392bf2720e17b1fbbf00
AnnabelZhang1 <azhang22@stuy.edu>
2020-10-26 19:18:03 -0400

    4 done

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class ArrayOps{
 
   public static int sum(int[] arr)
@@ -5,6 +7,7 @@ public class ArrayOps{
       int sum = 0;
       for (int i = 0; i < arr.length; i++)
         sum += arr[i];
+      System.out.println(sum);
       return sum;
     }
 
@@ -15,32 +18,54 @@ public class ArrayOps{
         {
           max = Math.max(arr[i], max);
         }
+      System.out.println(max);
       return max;
     }
 
   public static int[] sumRows(int[][] matrix)
     {
-      int[] arraysum = new int[matrix[0].length];
+      int[] arraysum = new int[matrix.length];
       for (int i = 0; i < matrix.length; i++)
         {
           int indivsum = 0;
-          for (int j = 0; j < matrix.length; j++)
+          for (int j = 0; j < matrix[0].length; j++)
             {
               indivsum += matrix[i][j];
             }
           arraysum[i] = indivsum;
         }
+      System.out.println(Arrays.toString(arraysum));
       return arraysum;
     }
-/*
+
   public static int[]largestInRows(int[][] matrix)
     {
-
+      int[] maxeach = new int[matrix.length];
+      for (int i = 0; i < matrix.length; i++)
+        {
+          int max = 0;
+          for (int j = 0; j < matrix[0].length; j++)
+            {
+              max = Math.max(matrix[i][j], max);
+            }
+          maxeach[i] = max;
+        }
+      System.out.println(Arrays.toString(maxeach));
+      return maxeach;
     }
 
-  public static sum(int[][] arr)
+  public static int sum(int[][] arr)
     {
-
+      int indivsum = 0;
+      for (int i = 0; i < arr.length; i++)
+        {
+          for (int j = 0; j < arr[0].length; j++)
+            {
+              indivsum += arr[i][j];
+            }
+        }
+      System.out.println(indivsum);
+      return indivsum;
     }
-*/
+
 }
--- Tester.java
+++ Tester.java
@@ -3,6 +3,16 @@ public class Tester{
     {
       ArrayOps.sum(new int[] {2,3,4});
       ArrayOps.largest(new int[] {3, 4, 10});
+      ArrayOps.sumRows(new int[][] {{  1,  0, 12, -1 },
+                                    {  7, -2,  2,  1 },
+                                    { -5, -2,  2, -9 }});
+      ArrayOps.largestInRows(new int[][] {{  1,  0, 12, -1 },
+                                          {  7, -2,  2,  1 },
+                                          { -5, -2,  2, -9 }});
+      ArrayOps.sum(new int[][] {{  1,  0, 12, -1 },
+                                {  7, -2,  2,  1 },
+                                { -5, -2,  2, -9 }});
+
     }
 
 
